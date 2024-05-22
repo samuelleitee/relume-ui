939b8bbf0e7aae0486db8c2f92cd57c142abbd02
samuelleitee <programmer.and.investor@gmail.com>
2024-05-21 22:19:27 -0300

    Relume UI

---
+++ src/components/Team/index.jsx
@@ -0,0 +1,17 @@
+import { TeamRoot } from "./TeamRoot";
+import { TeamHeadline } from "./TeamHeadline";
+import { TeamContent } from "./TeamContent";
+import { TeamCards } from "./TeamCards";
+import { TeamCard } from "./TeamCard";
+import { TeamSocialNetwork } from "./TeamSocialNertwork";
+import { TeamSocialNetworks } from "./TeamSocialNetworks";
+
+export const Team = {
+    Root: TeamRoot,
+    Headline: TeamHeadline,
+    Content: TeamContent,
+    Cards: TeamCards,
+    Card: TeamCard,
+    SocialNetworks: TeamSocialNetworks,
+    SocialNetwork: TeamSocialNetwork
+}
