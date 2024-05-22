e35c223538cba5d3bc728ae6a1bf455483ddc1f3
samuelleitee <programmer.and.investor@gmail.com>
2024-05-21 22:19:01 -0300

    Relume UI

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+// COMPONENTS
+import { Team } from "./components/Team";
+
+// ICONS
+import LinkedinIcon from "./assets/ic_linkedin.svg";
+import XIcon from "./assets/ic_x.svg";
+import DribbbleIcon from "./assets/ic_dribbble.svg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Team.Root>
+        <Team.Headline />
+
+        <Team.Content>
+          <Team.Cards>
+            <Team.Card
+              fullname="Samuel Leite Pereira"
+              jobTitle="Desenvolvedor Fullstack"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."
+            >
+              <Team.SocialNetworks>
+                <Team.SocialNetwork
+                  icon={LinkedinIcon}
+                  hyperlink="https://linkedin.com"
+                />
+                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
+                <Team.SocialNetwork
+                  icon={DribbbleIcon}
+                  hyperlink="https://dribbble.com"
+                />
+              </Team.SocialNetworks>
+            </Team.Card>
+            
+            <Team.Card
+              fullname="Paulo Bezerra"
+              jobTitle="Desenvolvedor Front-End"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."
+            >
+              <Team.SocialNetworks>
+                <Team.SocialNetwork
+                  icon={LinkedinIcon}
+                  hyperlink="https://linkedin.com"
+                />
+                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
+                <Team.SocialNetwork
+                  icon={DribbbleIcon}
+                  hyperlink="https://dribbble.com"
+                />
+              </Team.SocialNetworks>
+            </Team.Card>
+
+            <Team.Card
+              fullname="Lucas Correa"
+              jobTitle="Desenvolvedor Fullstack"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."
+            >
+              <Team.SocialNetworks>
+                <Team.SocialNetwork
+                  icon={LinkedinIcon}
+                  hyperlink="https://linkedin.com"
+                />
+                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
+                <Team.SocialNetwork
+                  icon={DribbbleIcon}
+                  hyperlink="https://dribbble.com"
+                />
+              </Team.SocialNetworks>
+            </Team.Card>
+
+            <Team.Card
+              fullname="Greice Santana"
+              jobTitle="Desenvolvedora Front-End"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."
+            >
+              <Team.SocialNetworks>
+                <Team.SocialNetwork
+                  icon={LinkedinIcon}
+                  hyperlink="https://linkedin.com"
+                />
+                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
+                <Team.SocialNetwork
+                  icon={DribbbleIcon}
+                  hyperlink="https://dribbble.com"
+                />
+              </Team.SocialNetworks>
+            </Team.Card>
+
+            <Team.Card
+              fullname="Milena"
+              jobTitle="Desenvolvedora Full-Stack"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."
+            >
+              <Team.SocialNetworks>
+                <Team.SocialNetwork
+                  icon={LinkedinIcon}
+                  hyperlink="https://linkedin.com"
+                />
+                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
+                <Team.SocialNetwork
+                  icon={DribbbleIcon}
+                  hyperlink="https://dribbble.com"
+                />
+              </Team.SocialNetworks>
+            </Team.Card>
+          </Team.Cards>
+        </Team.Content>
+      </Team.Root>
+    </>
+  );
+}
+
+export default App;
