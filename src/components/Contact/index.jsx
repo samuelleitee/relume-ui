7086321cb2b6aeed510fd8e8cb2b2a9fd0e43c7a
samuelleitee <programmer.and.investor@gmail.com>
2024-05-23 20:58:39 -0300

    Relume UI

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,17 @@
+import { ContactButton } from "./ContactButton";
+import { ContactForm } from "./ContactForm";
+import { ContactHeadline } from "./ContactHeadline";
+import { ContactInput } from "./ContactInput";
+import { ContactRoot } from "./ContactRoot";
+import { ContactTerms } from "./ContactTerms";
+import { ContactTextarea } from "./ContactTextarea";
+
+export const Contact = {
+    Root: ContactRoot,
+    Headline: ContactHeadline,
+    Form: ContactForm,
+    Input: ContactInput,
+    Textarea: ContactTextarea,
+    Terms: ContactTerms,
+    Button: ContactButton
+}
