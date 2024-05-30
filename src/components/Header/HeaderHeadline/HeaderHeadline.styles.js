f9883324e3641d6dcfbc5eba00b60e19c28806c0
samuelleitee <programmer.and.investor@gmail.com>
2024-05-29 22:15:25 -0300

    Relume UI

---
+++ src/components/Header/HeaderHeadline/HeaderHeadline.styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    color: #FFF;
+    font: var(--text-heading-h1);
+`
+
+export const Description = styled.p`
+    color: #FFF;
+    font: var(--text-medium-normal);
+`
+
+export const Container = styled.div`
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.5rem;
+
+    width: 100%;
+    max-width: 560px;
+`
