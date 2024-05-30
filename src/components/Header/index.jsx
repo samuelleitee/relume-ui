478c5266d2d12d3d44eb8a369b04b0f984545184
samuelleitee <programmer.and.investor@gmail.com>
2024-05-29 22:15:10 -0300

    Relume UI

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { HeaderActions } from "./HeaderActions";
+import { HeaderButton } from "./HeaderButton";
+import { HeaderHeadline } from "./HeaderHeadline";
+import { HeaderRoot } from "./HeaderRoot";
+
+export const Header = {
+    Root: HeaderRoot,
+    Headline: HeaderHeadline,
+    Button: HeaderButton,
+    Actions: HeaderActions
+}
