6eaf4c37c8095ea0db544045adb75ce3898c9c2e
samuelleitee <programmer.and.investor@gmail.com>
2024-05-29 22:15:30 -0300

    Relume UI

---
+++ src/components/Header/HeaderRoot/HeaderRoot.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1.5rem;
+
+    background: #808080;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 100vh;
+    width: 100%;
+    padding: 0 64px;
+`
