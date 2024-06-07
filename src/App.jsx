d421bb40f5fad014ad34b5820df2d89364c688e7
samuelleitee <programmer.and.investor@gmail.com>
2024-06-06 22:13:28 -0300

    Relume UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,135 @@ import XIcon from "./assets/ic_x.svg";
 import DribbbleIcon from "./assets/ic_dribbble.svg";
 
 import "./App.css";
+import React from "react";
 
 function App() {
+  const team = [
+    {
+      fullname: "Samuel Leite Pereira",
+      jobTitle: "Desenvolvedor Fullstack",
+      description: "Quem não sabe o que busca, não identifica o que acha.",
+      image: "https://avatars.githubusercontent.com/u/131712358?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+    {
+      fullname: "Paulo Bezerra",
+      jobTitle: "Desenvolvedor Front-End",
+      description:
+        "Caminhos tortuosos são apenas desafios para minha alma inquieta. Cada passo é uma história, cada aventura é uma lição.",
+      image: "https://avatars.githubusercontent.com/u/137209259?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+    {
+      fullname: "Lucas Correa",
+      jobTitle: "Desenvolvedor Fullstack",
+      description:
+        "Cada palavra é uma jornada, cada aprendizado é uma vitória.",
+      image: "https://avatars.githubusercontent.com/u/91700610?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+    {
+      fullname: "Greice Santana",
+      jobTitle: "Analista de Dados",
+      description:
+        "Minha vida é uma obra de arte em constante evolução, cada momento é uma nova inspiração.",
+      image: "https://avatars.githubusercontent.com/u/103227472?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+    {
+      fullname: "Milena Melo",
+      jobTitle: "Desenvolvedora Full-Stack",
+      description:
+        "Cada batalha é uma oportunidade de mudança, cada vitória é um passo em direção à justiça.",
+      image: "https://avatars.githubusercontent.com/u/148809944?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+    {
+      fullname: "Daniel",
+      jobTitle: "Analista de Dados",
+      description: "In God we trust. All others, bring me DATA.",
+      image: "https://avatars.githubusercontent.com/u/132000699?v=4",
+      SocialNetworks: [
+        {
+          icon: LinkedinIcon,
+          hyperlink: "https://linkedin.com",
+        },
+        {
+          icon: XIcon,
+          hyperlink: "https://x.com",
+        },
+        {
+          icon: DribbbleIcon,
+          hyperlink: "https://dribbble.com",
+        },
+      ],
+    },
+  ];
   return (
     <>
       <Team.Root>
@@ -18,119 +145,27 @@ function App() {
 
         <Team.Content>
           <Team.Cards>
-            <Team.Card
-              fullname="Samuel Leite Pereira"
-              jobTitle="Desenvolvedor Fullstack"
-              description="Quem não sabe o que busca, não identifica o que acha."
-              image="https://avatars.githubusercontent.com/u/131712358?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
-
-            <Team.Card
-              fullname="Paulo Bezerra"
-              jobTitle="Desenvolvedor Front-End"
-              description="Caminhos tortuosos são apenas desafios para minha alma inquieta. Cada passo é uma história, cada aventura é uma lição."
-              image="https://avatars.githubusercontent.com/u/137209259?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
-
-            <Team.Card
-              fullname="Lucas Correa"
-              jobTitle="Desenvolvedor Fullstack"
-              description=" Cada palavra é uma jornada, cada aprendizado é uma vitória."
-              image="https://avatars.githubusercontent.com/u/91700610?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
-
-            <Team.Card
-              fullname="Greice Santana"
-              jobTitle="Analista de Dados"
-              description="Minha vida é uma obra de arte em constante evolução, cada momento é uma nova inspiração."
-              image="https://avatars.githubusercontent.com/u/103227472?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
-
-            <Team.Card
-              fullname="Milena Melo"
-              jobTitle="Desenvolvedora Full-Stack"
-              description="Cada batalha é uma oportunidade de mudança, cada vitória é um passo em direção à justiça."
-              image="https://avatars.githubusercontent.com/u/148809944?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
-
-            <Team.Card
-              fullname="Daniel"
-              jobTitle="Analista de Dados"
-              description="In God we trust. All others, bring me DATA."
-              image="https://avatars.githubusercontent.com/u/132000699?v=4"
-            >
-              <Team.SocialNetworks>
-                <Team.SocialNetwork
-                  icon={LinkedinIcon}
-                  hyperlink="https://linkedin.com"
-                />
-                <Team.SocialNetwork icon={XIcon} hyperlink="https://x.com" />
-                <Team.SocialNetwork
-                  icon={DribbbleIcon}
-                  hyperlink="https://dribbble.com"
-                />
-              </Team.SocialNetworks>
-            </Team.Card>
+            {team.map((integrant, key) => (
+              <React.Fragment key={key}>
+                <Team.Card
+                  fullname={integrant.fullname}
+                  jobTitle={integrant.jobTitle}
+                  description={integrant.description}
+                  image={integrant.image}
+                >
+                  <Team.SocialNetworks>
+                    {integrant.SocialNetworks.map((socialNetwork, key) => (
+                      <React.Fragment key={key}>
+                        <Team.SocialNetwork
+                          icon={socialNetwork.icon}
+                          hyperlink={socialNetwork.hyperlink}
+                        />
+                      </React.Fragment>
+                    ))}
+                  </Team.SocialNetworks>
+                </Team.Card>
+              </React.Fragment>
+            ))}
           </Team.Cards>
         </Team.Content>
       </Team.Root>
@@ -147,11 +182,21 @@ function App() {
       </Contact.Root>
 
       <Header.Root>
-        <Header.Headline title="Medium length hero headline goes here" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat." />
+        <Header.Headline
+          title="Medium length hero headline goes here"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."
+        />
 
         <Header.Actions>
-          <Header.Button primary text="Submit" action={() => console.log("Submit Button")} />
-          <Header.Button text="Cancel" action={() => console.log("Cancel Button")} />
+          <Header.Button
+            primary
+            text="Submit"
+            action={() => console.log("Submit Button")}
+          />
+          <Header.Button
+            text="Cancel"
+            action={() => console.log("Cancel Button")}
+          />
         </Header.Actions>
       </Header.Root>
     </>
