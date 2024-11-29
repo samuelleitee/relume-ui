2551aa8f467a62fb7d91269529329e3ebf61ca24
samuelleitee <programmer.and.investor@gmail.com>
2024-11-29 11:08:15 -0300

    Relume UI

---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,9 @@
+import { GalleryImage } from "./GalleryImage";
+import { GalleryRoot } from "./GalleryRoot";
+import { GalleryTitle } from "./GalleryTitle";
+
+export const Gallery = {
+    Root: GalleryRoot,
+    Title: GalleryTitle,
+    Image: GalleryImage
+}
